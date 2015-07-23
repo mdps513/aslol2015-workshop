5691c0a06439ee104195c35d9c0bc504907c0088
KaoriYatsu <kaori@wlan4010.u-aizu.ac.jp>
2015-07-23 16:14:07 +0900

    add post receiving method

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.post('/exec', function (req, res) {
+  //res.send('Hello World!');
+  console.log(req.body);
 });
 
 var server = app.listen(3000, function () {
